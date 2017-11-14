a2c82cb0f74b0b6857de4b0062bbef2742b38d75
nikedia <nikkediana@gmail.com>
2017-11-14 06:39:07 -0500

    subiendo css y js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,51 @@
+
+var showHide = function(e) {
+    var tabselected = e.target.dataset.tabselected;
+
+    //Create var to store their values.
+    var breakfast = document.getElementById('breakfast');
+    var lunch = document.getElementById('lunch');
+    var dinner = document.getElementById('dinner');
+  
+        if(tabselected === 'tabBreakfast'){
+            console.log('vamos a desayunar');
+            //Ocultar almuerzo y cena.
+            lunch.style.display = 'none';
+            dinner.style.display = 'none';
+            //Mostrar desayuno.
+            breakfast.style.display = 'block';
+        } else if (tabselected === 'tabLunch'){
+            console.log('vamos a almorzar');
+            //Ocultar desayuno y cena.
+            breakfast.style.display = 'none';
+            dinner.style.display = 'none';
+            //Mostrar almuerzo.
+            lunch.style.display = 'block';
+        } else if (tabselected === 'tabDinner'){
+            console.log('vamos a cenar');
+            //Ocultar desayuno y almubreakfast.style.display = 'none'erzo.
+            breakfast.style.display = 'none';
+            lunch.style.display = 'none';
+            //Mostrar cena.
+            dinner.style.display = 'block';
+        }
+};
+
+//To load page when we open the web.
 var loadPage = function(){
-    var 
-}
+    //to hide all dishes when loading page.
+    var breakfast = document.getElementById('breakfast');
+    var lunch = document.getElementById('lunch');
+    var dinner = document.getElementById('dinner');
+
+    breakfast.style.display = 'none';
+    lunch.style.display = 'none';
+    dinner.style.display = 'none';
+    //to get buttons elements.
+    var elementsTab = document.getElementsByClassName('tab');
+        for(var i = 0; i<elementsTab.length; i++){
+            elementsTab[i].addEventListener('click', showHide);
+        }
+};
+
+loadPage(); //calling function.
